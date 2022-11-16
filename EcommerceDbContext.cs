67b8e73ec1223ea28a060b3b0697de0a9c6e073b
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-16 15:26:26 +0100

    Aggiungere i file di progetto.

---
+++ EcommerceDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+
+public class EcommerceDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=ecommerce-db;Integrated Security=True;Encrypt=false;");
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,22 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime Date { get; set; }
+    public int Amount { get; set; }
+    public bool Status { get; set; }
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; }
+
+    public List<Payment> Payments { get; set; }
+
+    public List<Product> Products { get; set; }
+}
